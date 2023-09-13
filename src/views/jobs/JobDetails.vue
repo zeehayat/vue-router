dc902c0286cc4e4c4e06f9365745f216ca11eda2
zeehayat <zeenux@gmail.com>
2023-09-13 21:06:12 +0500

    3 files modified

--- src/views/jobs/JobDetails.vue
+++ src/views/jobs/JobDetails.vue
@@ -1,10 +1,16 @@
 <template>
   <h1>Job Details</h1>
-  <p>The Job Id is {{ $route.params.id }}</p>
+  <p>The Job Id is {{ id }}</p>
 </template>
 
 <script>
 export default {
   name: 'JobDetails',
-};
+  props:['id'],
+  data() {
+    return {
+      //ids: this.$route.params.id,
+    }
+  },
+}
 </script>
