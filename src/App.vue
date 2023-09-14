0500c7eba9b419367baaaee2875f87db18f5f366
zeehayat <zeenux@gmail.com>
2023-09-14 16:23:40 +0500

    2 files modified, 1 added

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <template>
   <div id="nav">
-    <router-link class='nav-item' to="/">Home</router-link>
-    <router-link class='nav-item' to="/about">About</router-link>
-    <router-link class='nav-item' :to="{name: 'Jobs'}">Jobs</router-link>
+    <router-link class="nav-item" to="/">Home</router-link>
+    <router-link class="nav-item" to="/about">About</router-link>
+    <router-link class="nav-item" :to="{ name: 'Jobs' }">Jobs</router-link>
   </div>
-  <div class='btn-group'>
-  <button @click="redirect">Redirect</button>
-  <button @click="back">Go Back</button>
-  <button @click="forward">Forward</button>
+  <div class="btn-group">
+    <button @click="redirect">Redirect</button>
+    <button @click="back">Go Back</button>
+    <button @click="forward">Forward</button>
   </div>
 
   <router-view />
@@ -17,17 +17,17 @@
 export default {
   name: 'App',
   components: {},
-  methods:{
-    redirect(){
-
+  methods: {
+    redirect() {
+      this.$router.push({ name: 'Home' });
     },
-    forward(){
-
+    forward() {
+      this.$router.go(1);
     },
-    back(){
-      this.$router.back(-1)
-    }
-  }
+    back() {
+      this.$router.go(-1);
+    },
+  },
 };
 </script>
 
@@ -40,21 +40,21 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
-.nav-item{
-  margin:10px;
-  padding:5px;
-  border:1px solid gray;
-  text-decoration:none;
-  border-radius:5px;
-  border-bottom:none;
+.nav-item {
+  margin: 10px;
+  padding: 5px;
+  border: 1px solid gray;
+  text-decoration: none;
+  border-radius: 5px;
+  border-bottom: none;
 }
-.btn-group{
-  margin:20px;
+.btn-group {
+  margin: 20px;
 }
 button {
   margin: 0 10px;
-  padding:10px;
-  border:none;
-  border-radius:4px;
+  padding: 10px;
+  border: none;
+  border-radius: 4px;
 }
 </style>
